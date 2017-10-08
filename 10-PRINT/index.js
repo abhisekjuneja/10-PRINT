be8c53a2bebac6d21fa2c6658504ea6b162ade78
abhisekjuneja <abhisekjuneja@gmail.com>
2017-10-09 00:09:22 +0530

    Completed '10-Print' Coding Challenge

--- 10-PRINT/index.js
+++ 10-PRINT/index.js
@@ -1,22 +1,38 @@
 let sketchReloadButton = document.getElementById('reloadSketch');
+let sketchHolder = document.getElementById('sketch-holder');
 let sketchFrame = document.querySelector('.sketch');
 let windowReloadButton = document.getElementById('reloadWindow');
 let scaleFactorSlider = document.getElementById('scaleFactor');
 let stochasticFactorSlider = document.getElementById('stochasticFactor');
+let openControlPanelButton = document.getElementById('openControlPanel');
+let controlPanel = document.getElementById('control-panel');
+let controlPanelOpenStatus = false;
 
 window.onload = () => {
     scaleFactorSlider.value = 50;
     stochasticFactorSlider.vale = 0.5;
 }
 
-sketchReloadButton.addEventListener('click', () => {
-    sketchFrame.contentWindow.location.reload(true);
-    sketchFrame.scrollIntoView();
+openControlPanelButton.addEventListener('click', ()=> {
+    if(controlPanelOpenStatus){
+        controlPanel.style.display = 'none';
+        sketchHolder.classList.remove('col-md-8');
+        sketchHolder.classList.add('col-md-12');
+        controlPanelOpenStatus = false;
+        openControlPanelButton.innerText = 'Open Control Panel';
+    }
+    else{
+        sketchHolder.classList.remove('col-md-12');
+        sketchHolder.classList.add('col-md-8');
+        controlPanel.style.display = 'block';
+        controlPanelOpenStatus = true;
+        openControlPanelButton.innerText = 'Close Control Panel';
+    }
 });
 
-
-windowReloadButton.addEventListener('click', () => {
-    window.location.reload(true);
+sketchReloadButton.addEventListener('click', () => {
+    sketchFrame.contentWindow.location.reload(true);
+    document.getElementById('sketch-holder').scrollIntoView();
 });
 
 scaleFactorSlider.addEventListener('change', controlPanelSettingsChanged);
